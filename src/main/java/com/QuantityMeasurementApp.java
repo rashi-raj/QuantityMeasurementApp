0c65007962a708645eee77b3ea454cb247cfc725
rashi0122 <rashiraj847@gmail.com>
2026-02-22 22:22:43 +0530

    [Rashi Raj] : Implemented Unit to Unit conversion

--- src/main/java/com/QuantityMeasurementApp.java
+++ src/main/java/com/QuantityMeasurementApp.java
@@ -2,38 +2,52 @@ package com;
 
 public class QuantityMeasurementApp {
 
-    public static boolean demonstrateLengthEquality(Length length1, Length length2) {
-        boolean result = length1.equals(length2);
-        System.out.println("Comparing: " + length1 + " and " + length2);
-        System.out.println("Are equal? -> " + result);
+    public static boolean demonstrateLengthEquality(Length l1, Length l2) { 
+        return l1.equals(l2);
+    }
+
+    public static boolean demonstrateLengthComparison(double value1, LengthUnit unit1, double value2, LengthUnit unit2) {
+
+        Length l1 = new Length(value1, unit1);
+        Length l2 = new Length(value2, unit2);
+
+        boolean result = l1.equals(l2);
+
+        System.out.println("lengths are equal : " + result);
         return result;
     }
 
-    public static boolean demonstrateLengthComparison(double value1, LengthUnit unit1,
-                                                      double value2, LengthUnit unit2) {
+    public static double demonstrateLengthConversion(double value, LengthUnit from, LengthUnit to) {
 
-        Length length1 = new Length(value1, unit1);
-        Length length2 = new Length(value2, unit2);
+        double result = Length.convert(value, from, to);
 
-        return demonstrateLengthEquality(length1, length2);
+        System.out.println("Input: convert(" + value + ", "
+                + from + ", " + to + ") → Output: " + result);
+
+        return result;
     }
-    
+
+    public static Length demonstrateLengthConversion(Length length, LengthUnit toUnit) {
+
+        Length result = length.convertTo(toUnit);
+
+        System.out.println("Input: convert(" + length.getValue() + ", "
+                + length.getUnit() + ", " + toUnit + ") → Output: "
+                + result.getValue());
+
+        return result;
+    }
 
     public static void main(String[] args) {
 
-        // Demonstrate Feet and Inches comparison
-        demonstrateLengthComparison(1.0, LengthUnit.FEET, 12.0, LengthUnit.INCHES);
+        demonstrateLengthConversion(1.0,LengthUnit.FEET,LengthUnit.INCHES);
 
-        // Demonstrate Yards and Inches comparison
-        demonstrateLengthComparison(1.0, LengthUnit.YARDS, 36.0, LengthUnit.INCHES);
+        demonstrateLengthConversion(3.0,LengthUnit.YARDS,LengthUnit.FEET);
 
-        // Demonstrate Centimeters and Inches comparison
-        demonstrateLengthComparison(100.0, LengthUnit.CENTIMETERS, 39.3701, LengthUnit.INCHES);
+        demonstrateLengthConversion(36.0,LengthUnit.INCHES,LengthUnit.YARDS);
 
-        // Demonstrate Feet and Yards comparison
-        demonstrateLengthComparison(3.0, LengthUnit.FEET, 1.0, LengthUnit.YARDS);
+        demonstrateLengthConversion(1.0,LengthUnit.CENTIMETERS,LengthUnit.INCHES);
 
-        // Demonstrate Centimeters and Feet comparison
-        demonstrateLengthComparison(30.48, LengthUnit.CENTIMETERS, 1.0, LengthUnit.FEET);
+        demonstrateLengthConversion(0.0,LengthUnit.FEET,LengthUnit.INCHES);
     }
 }
