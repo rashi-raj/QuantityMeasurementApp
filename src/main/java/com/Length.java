b7a4cb3d206f634842050d662eaeff76d385a25a
rashi0122 <rashiraj847@gmail.com>
2026-02-22 19:13:45 +0530

    [Rashi Raj] : Add extended unit support with Yards and Centimeters

--- src/main/java/com/Length.java
+++ src/main/java/com/Length.java
@@ -29,5 +29,25 @@ public class Length {
 
 	        return Double.compare(this.convertToBaseUnit(), that.convertToBaseUnit()) == 0;
 	    }
+	 
+	 public static void main(String[] args) {
+		 Length length1 = new Length(1.0, LengthUnit.FEET);
+		 Length length2 = new Length(12.0, LengthUnit.INCHES);
+		 System.out.println("Are lengths equal? " + length1.equals(length2));
+		 
+		 Length length3 = new Length(1.0, LengthUnit.YARDS);
+		 Length length4 = new Length(36.0, LengthUnit.INCHES);
+		 System.out.println("Are lengths equal? " + length3.equals(length4));
+		 
+		 Length length5 = new Length(100.0, LengthUnit.CENTIMETERS);
+		 Length length6 = new Length(39.3701, LengthUnit.INCHES);
+		 System.out.println("Are lengths equal? " + length5.equals(length6));
+		
+	 }
+
+	 @Override
+	 public String toString() {
+		return value + " " + unit;
+	 }
 
 }
--- src/main/java/com/QuantityMeasurementApp.java
+++ src/main/java/com/QuantityMeasurementApp.java
@@ -2,12 +2,38 @@ package com;
 
 public class QuantityMeasurementApp {
 
-	public static void main(String[] args) {
+    public static boolean demonstrateLengthEquality(Length length1, Length length2) {
+        boolean result = length1.equals(length2);
+        System.out.println("Comparing: " + length1 + " and " + length2);
+        System.out.println("Are equal? -> " + result);
+        return result;
+    }
 
-        Length length1 = new Length(1.0, LengthUnit.FEET);
-        Length length2 = new Length(12.0, LengthUnit.INCHES);
+    public static boolean demonstrateLengthComparison(double value1, LengthUnit unit1,
+                                                      double value2, LengthUnit unit2) {
 
-        System.out.println("Are lengths equal? " + length1.equals(length2));
+        Length length1 = new Length(value1, unit1);
+        Length length2 = new Length(value2, unit2);
+
+        return demonstrateLengthEquality(length1, length2);
+    }
+    
+
+    public static void main(String[] args) {
+
+        // Demonstrate Feet and Inches comparison
+        demonstrateLengthComparison(1.0, LengthUnit.FEET, 12.0, LengthUnit.INCHES);
+
+        // Demonstrate Yards and Inches comparison
+        demonstrateLengthComparison(1.0, LengthUnit.YARDS, 36.0, LengthUnit.INCHES);
+
+        // Demonstrate Centimeters and Inches comparison
+        demonstrateLengthComparison(100.0, LengthUnit.CENTIMETERS, 39.3701, LengthUnit.INCHES);
+
+        // Demonstrate Feet and Yards comparison
+        demonstrateLengthComparison(3.0, LengthUnit.FEET, 1.0, LengthUnit.YARDS);
+
+        // Demonstrate Centimeters and Feet comparison
+        demonstrateLengthComparison(30.48, LengthUnit.CENTIMETERS, 1.0, LengthUnit.FEET);
     }
 }
-
