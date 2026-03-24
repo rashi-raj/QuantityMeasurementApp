dee61f136644dbcada6c3d25b84e4e08f4328331
rashi0122 <rashiraj847@gmail.com>
2026-03-24 11:13:00 +0530

    [Rashi Raj] : Added UC17 implementation

---
+++ src/main/java/com/app/quantitymeasurement/QuantityMeasurementAppApplication.java
@@ -0,0 +1,17 @@
+package com.app.quantitymeasurement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Quantity Measurement API", version = "1.0.0", description = "REST API for quantity measurement operations — UC17 Spring Boot"))
+
+public class QuantityMeasurementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuantityMeasurementAppApplication.class, args);
+	}
+}
